64420ea67b40f717d67901829565dfefc1ce26d0
Kimberley Goodman <40457190@live.napier.ac.uk>
2022-07-14 21:10:51 +0100

    Added classes so far for Business Layer and Testing folder.

---
+++ NMB/Business Layer/Email.cs
@@ -0,0 +1,12 @@
+﻿ilusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMB.Business_Layer
+{
+    class Email
+    {
+    }
+}
