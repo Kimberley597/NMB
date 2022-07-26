a51c151e6a7198e3cc914315403d604faa8edcc6
Kimberley Goodman <40457190@live.napier.ac.uk>
2022-07-26 17:11:43 +0100

    Added functionality to read in textwords csv file and add in adbreviations to tweet messages and SMS messages.

--- NMB/Business Layer/Tweet.cs
+++ NMB/Business Layer/Tweet.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace NMB.Business_Layer
 {
-    class Tweet
+    //Inheriting from MessageResponseType class
+    public class Tweet : MessageResponseType
     {
+
+        //Declare dictionary for text speak
+        Dictionary<string, string> TextSpeechDict;
+
+        //constructor
+        public Tweet(string rawMsg, Dictionary<string, string> textSpeechDict, int maxLength = 140) : base(rawMsg)
+        {
+            //declare variables
+            this.TextSpeechDict = textSpeechDict;
+            //Find where message section starts of message string
+            int messageStart = rawMessage.LastIndexOf("-") + 1;
+            //Find the message section length by subtracting messageSTart from full message
+            int messageLength = rawMessage.Length - 1 - messageStart;
+            //If the message length is longer than 140 character, cut it at that length
+            if (messageLength > maxLength)
+            {
+                rawMessage = rawMessage.Substring(0, messageStart + maxLength) + ";";
+            }
+        }
+
+        //overiding function
+        //Here we take in the string and look for any text speak, if found, add in expanded meaning
+        public override List<string> ProcessMessage()
+        {
+            //change message to upper case
+            string upperCaseRawMessage = rawMessage.ToUpper();
+
+            //foreach loop to check for any keys present (abbreviations) are found
+            foreach (KeyValuePair<string, string> word in TextSpeechDict)
+            {
+                //If the message contains one of the keys, check how many times it is present
+                if (upperCaseRawMessage.Contains(word.Key))
+                {
+                    List<int> indexes = Utility.GetAllInstancesInString(upperCaseRawMessage, word.Key);
+
+                    //foreach time the key is present, insert the meaning (value) after the abbreviation (key)
+                    foreach (int index in indexes)
+                    {
+                        upperCaseRawMessage = upperCaseRawMessage.Insert(index + word.Key.Length, " <" + word.Value + "> ");
+                    }
+                }
+            }
+
+            processedMessage = upperCaseRawMessage;
+            return null;
+        }
+
+        public virtual string Serialise()
+        {
+            return null;
+        }
+
     }
 }
