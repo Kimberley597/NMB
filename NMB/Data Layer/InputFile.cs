252509ff59ac01d2e9ff80befa76980deef42ae8
Kimberley Goodman <40457190@live.napier.ac.uk>
2022-08-17 16:40:50 +0100

    Added class diagram and class to the data layer for processing an uploaded file containing a message.

---
+++ NMB/Data Layer/InputFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMB.Data_Layer
+{
+    //ToDo: class for uploading a file with a message to then categorise, sanitise and validate
+    class InputFile
+    {
+    }
+}
